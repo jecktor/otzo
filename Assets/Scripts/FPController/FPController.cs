4976173777445270e12f118f4c2230c5499f6b83
jecktor <vek@unsteady.life>
2025-10-10 01:33:38 -0600

    feat: basic character controller

---
+++ Assets/Scripts/FPController/FPController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+namespace Otzo {
+	[RequireComponent(typeof(CharacterController))]
+	
+	public class FPController : MonoBehaviour
+	{
+		[Header("Movement Parameters")]
+		public float MaxSpeed = 3.5f;
+		public float Acceleration = 15f;
+		
+		public Vector3 CurrentVelocity { get; private set; }
+		public float CurrentSpeed { get; private set; }
+		
+		[Header("Looking Parameters")]
+		public Vector2 LookSensitivity = new Vector2(0.1f, 0.1f);
+		
+		public float PitchLimit = 85f;
+		
+		[SerializeField] float currentPitch = 0f;
+		
+		public float CurrentPitch
+		{
+			get => currentPitch;
+			set
+			{
+				currentPitch = Mathf.Clamp(value, -PitchLimit, PitchLimit);
+			}
+		}
+		
+		[Header("Input")]
+		public Vector2 MoveInput;
+		public Vector2 LookInput;
+		
+		[Header("Components")]
+		[SerializeField] CinemachineCamera fpCamera;
+		[SerializeField] CharacterController characterController;
+		
+		void onValidate()
+		{
+			if (characterController == null)
+			{
+				characterController = GetComponent<CharacterController>();
+			}
+		}
+		
+
+		void Update()
+		{
+			MoveUpdate();
+			LookUpdate();
+		}
+		
+		void MoveUpdate()
+		{
+			Vector3 motion = transform.forward * MoveInput.y + transform.right * MoveInput.x;
+			motion.y = 0f;
+			motion.Normalize();
+			
+			if (motion.sqrMagnitude >= 0.01f)
+			{
+				CurrentVelocity = Vector3.MoveTowards(CurrentVelocity, motion * MaxSpeed, Acceleration * Time.deltaTime);
+			}
+			else
+			{
+				CurrentVelocity = Vector3.MoveTowards(CurrentVelocity, Vector3.zero, Acceleration * Time.deltaTime);
+			}
+			
+			float verticalVelocity = Physics.gravity.y * 20f * Time.deltaTime;
+			
+			Vector3 fullVelocity = new Vector3(CurrentVelocity.x, verticalVelocity, CurrentVelocity.z);
+			
+			characterController.Move(fullVelocity * Time.deltaTime);
+			
+			CurrentSpeed = CurrentVelocity.magnitude;
+		}
+		
+		void LookUpdate()
+		{
+			Vector2 input = new Vector2(LookInput.x * LookSensitivity.x, LookInput.y * LookSensitivity.y);
+			
+			CurrentPitch -= input.y;
+			
+			fpCamera.transform.localRotation = Quaternion.Euler(CurrentPitch, 0f, 0f);
+			
+			transform.Rotate(Vector3.up * input.x);
+		}
+	}
+}
---
+++ Assets/Scripts/FPPlayer/FPPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Otzo {
+	[RequireComponent(typeof(FPController))]
+	
+	public class Player : MonoBehaviour
+	{
+		[Header("Components")]
+		[SerializeField] FPController FPController;
+		
+		void OnMove(InputValue value)
+		{
+			FPController.MoveInput = value.Get<Vector2>();
+		}
+		
+		void OnLook(InputValue value)
+		{
+			FPController.LookInput = value.Get<Vector2>();
+		}
+		
+		void OnValidate()
+		{
+			if (FPController == null) FPController = GetComponent<FPController>();
+		}
+		
+		void Start()
+		{
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+}
