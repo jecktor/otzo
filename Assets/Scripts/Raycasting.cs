7616ecdd140a1359606423dfc3cb3b9e6f610d51
jecktor <vek@unsteady.life>
2025-10-18 00:35:18 -0600

    feat: basic raycasting

---
+++ Assets/Scripts/Raycasting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Otzo
+{
+	public class Raycasting : MonoBehaviour
+	{
+		public static GameObject TargetObject;
+
+		void Update()
+		{
+			if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+			{
+				TargetObject = hit.collider.gameObject;
+			}
+			else
+			{
+				TargetObject = null;
+			}
+		}
+	}
+}
