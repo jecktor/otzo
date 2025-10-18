aaf6460818de333b58628571feca0818d90b7597
julioerives3452 <julioerives3452@gmail.com>
2025-10-18 12:32:18 -0600

    feat: add alert to sleep

--- Assets/Scripts/triggers/BedTriggerSleep.cs
+++ Assets/Scripts/triggers/BedTriggerSleep.cs
@@ -1,24 +1,22 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class BedTriggerSleep : MonoBehaviour
 {
     [Header("Configuración Cama")]
     public KeyCode interactionKey = KeyCode.E;
     public float interactionRange = 2f;
-    public GameObject interactionUI; // UI que muestra "Presiona E para dormir"
 
     private Transform player;
     private bool isNearBed = false;
     private GameClock gameClock;
+    private GUIStyle guiStyle;
+    private Texture2D backgroundTex;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         gameClock = GameClock.Instance;
-
-        if (interactionUI != null)
-            interactionUI.SetActive(false);
+        CreateGUIStyle();
     }
 
     void Update()
@@ -27,22 +25,9 @@ public class BedTriggerSleep : MonoBehaviour
 
         CheckBedProximity();
 
-        if (isNearBed)
+        if (isNearBed && Input.GetKeyDown(interactionKey))
         {
-            if (Input.GetKeyDown(interactionKey))
-            {
-                SleepInBed();
-            }
-
-            // Mostrar UI de interacción
-            if (interactionUI != null)
-                interactionUI.SetActive(true);
-        }
-        else
-        {
-            // Ocultar UI de interacción
-            if (interactionUI != null)
-                interactionUI.SetActive(false);
+            SleepInBed();
         }
     }
 
@@ -54,15 +39,38 @@ public class BedTriggerSleep : MonoBehaviour
 
     void SleepInBed()
     {
-        Debug.Log("Durmiendo en la cama...");
 
         if (gameClock != null)
         {
             gameClock.SleepAndAdvanceTime();
         }
-        else
+
+    }
+
+    void CreateGUIStyle()
+    {
+        guiStyle = new GUIStyle();
+        guiStyle.normal.textColor = Color.white;
+        guiStyle.fontSize = 20;
+        guiStyle.alignment = TextAnchor.MiddleCenter;
+        guiStyle.fontStyle = FontStyle.Bold;
+
+        backgroundTex = new Texture2D(1, 1);
+        backgroundTex.SetPixel(0, 0, new Color(0, 0, 0, 0.7f));
+        backgroundTex.Apply();
+        guiStyle.normal.background = backgroundTex;
+    }
+
+    void OnGUI()
+    {
+        if (isNearBed)
         {
-            Debug.LogError("GameClock no encontrado!");
+            float boxWidth = 350;
+            float boxHeight = 50;
+            float x = (Screen.width - boxWidth) / 2;
+            float y = 100;
+
+            GUI.Box(new Rect(x, y, boxWidth, boxHeight), "Presiona la letra E para dormir", guiStyle);
         }
     }
 
@@ -70,5 +78,11 @@ public class BedTriggerSleep : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, interactionRange);
+    }
+
+    void OnDestroy()
+    {
+        if (backgroundTex != null)
+            Destroy(backgroundTex);
     }
 }
